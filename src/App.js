cce6c7f94069c1f5a78f86446799f377f70ba404
gokce <gokcetascii@gmail.com>
2023-12-08 02:57:19 +0300

    profile page path added with react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Header/>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/' element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
+
+  );
+}
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
   );
 }
 
