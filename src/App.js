a071199db6f5dfa3f696d6e5daf2ee64eafb0d1e
gokce <gokcetascii@gmail.com>
2023-12-12 00:52:22 +0300

    messages button, intercom page and style edit added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import './App.css';
+import React, { useState } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProfileDetail from './components/ProfileDetail';
+import { IoIosArrowDown } from "react-icons/io";
+import { BiSolidMessageSquareDots } from "react-icons/bi";
+import Intercom from './components/Intercom';
 
 function App() {
+  const [intercomState, setIntercomState] = useState({
+    isIntercomOpen: false,
+    isArrowDownVisible: false,
+  });
+
+  const handleIntercomToggle = () => {
+    console.log('Toggling Intercom');
+    setIntercomState((prevState) => ({
+      isIntercomOpen: !prevState.isIntercomOpen,
+      isArrowDownVisible: !prevState.isArrowDownVisible,
+    }));
+  };
+
   return (
     <Router>
       <div>
+        <Header />
+        <ProfileDetail />
+        <Footer />
+        <div className={`fixed bottom-24 right-7`}>
+          {intercomState.isIntercomOpen && <Intercom onClose={handleIntercomToggle} />}
+          <button
+            onClick={handleIntercomToggle}
+            className="bg-primary text-white rounded-full flex justify-center items-center w-12 h-12 hover:scale-110 transition duration-300 ease-in-out"
+            style={{ position: 'fixed', bottom: '2rem', right: '1rem' }}
+          >
+            {intercomState.isArrowDownVisible ? (
+              <IoIosArrowDown className='w-6 h-6' />
+            ) : (
+              <BiSolidMessageSquareDots className='w-6 h-6' />
+            )}
+          </button>
+        </div>
+
         <Routes>
-          <Route path='/' element={<ProfilePage />} />
+          <Route
+            path='/'
+            element={
+              <>
+                <ProfilePage />
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
-
   );
 }
+
 
 const ProfilePage = () => {
   return (
     <div>
-      <Header />
-      <ProfileDetail/>
-      <Footer/>
     </div>
   );
 }
